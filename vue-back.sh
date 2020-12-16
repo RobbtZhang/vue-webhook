5244b689728c365343863e9bd8f5ef96fc539cec
张应生 <652057507@qq.com>
2020-12-16 21:13:38 +0800

    v1.0.2

--- vue-back.sh
+++ vue-back.sh
@@ -7,9 +7,9 @@ git clean -f
 echo "拉取最新代码"
 git pull origin master
 echo "开始执行构建"
-docker build -t blog-server-koa2:1.0.1 .
+docker build -t blog-server-koa2:1.0.2 .
 echo "停止旧容器并删除旧容器"
 docker stop blog-server-koa2-container
 docker rm vue-back-container
 echo "启动新容器"
-docker container run -p 3000:3000 --name blog-server-koa2-container -d blog-server-koa2:1.0.1
+docker container run -p 3000:3000 --name blog-server-koa2-container -d blog-server-koa2:1.0.2
