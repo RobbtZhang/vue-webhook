851a6c93ee95228044fa2b58ea64a8284971a66d
张应生 <652057507@qq.com>
2020-12-21 21:26:18 +0800

    edu finish

--- webhook.js
+++ webhook.js
@@ -1,5 +1,6 @@
 let http = require('http')
 let crypto = require('crypto')
+let { spawn } = require('child_process')
 let SECRET = '123456'
 function sign(body) {
   return 'sha1=' + crypto.createHmac('sha1',SECRET).update(body).digest('hex')
@@ -23,6 +24,15 @@ let server = http.createServer(function(req,res){
       res.end(JSON.stringify({ok:true}))
       if(event == 'push'){ // 开始部署
         let payload = JSON.parse(body)
+        let child = spawn('sh',[`./${payload.repository.name}.sh`])
+        let buffers = []
+        child.stdout.on('data', function(){
+          buffers.push(buffer)
+        })
+        child.stdout.on('end', function(){
+          let log = Buffer.concat(buffers)
+          console.log(log)
+        })
       }
     })
   }else {
