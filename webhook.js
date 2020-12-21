e1278c386b06739a47f5fc63a7240b33bc3bcf33
张应生 <652057507@qq.com>
2020-12-21 21:54:01 +0800

    edu finish test1

--- webhook.js
+++ webhook.js
@@ -3,7 +3,7 @@ let crypto = require('crypto')
 let { spawn } = require('child_process')
 let SECRET = '123456'
 function sign(body) {
-  return 'sha1=' + crypto.createHmac('sha1',SECRET).update(body).digest('hex')
+  return `sha1=` + crypto.createHmac('sha1',SECRET).update(body).digest('hex')
 }
 let server = http.createServer(function(req,res){
   console.log(req.method,req.url)
@@ -14,23 +14,23 @@ let server = http.createServer(function(req,res){
     })
     req.on('end',function(buffer){
       let body = Buffer.concat(buffers)
-      let event = req.headers['x-gitHub-event'] // event=push
+      let event = req.headers['X-github-event'] // event=push
       // github 请求来的时候 要传递请求体body 另外还会传一个 signature 过来 需要验证签名
       let signature = req.headers['x-hub-signature']
       if(signature !== sign(body)){
         return res.end('Not Allowed')
       }
-      res.setHeader('Content-Type','application/json')
-      res.end(JSON.stringify({ok:true}))
-      if(event == 'push'){ // 开始部署
+      res.setHeader('Content-Type', 'application/json')
+      res.end(JSON.stringify({ok: true}))
+      if(event === 'push'){// 开始部署
         let payload = JSON.parse(body)
-        let child = spawn('sh',[`./${payload.repository.name}.sh`])
+        let child = spawn('sh', [`./${payload.repository.name}.sh`])
         let buffers = []
         child.stdout.on('data', function(buffer){
           buffers.push(buffer)
         })
-        child.stdout.on('end', function(buffer){
-          let log = Buffer.concat(buffers)
+        child.stdout.on("end", function(buffer){
+          let log = Buffer.concat(buffers).toString()
           console.log(log)
         })
       }
